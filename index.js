9d5802c7aec369bc56c6c422c45bf3446fe32d1e
Bestun Gardi <bestun95@yahoo.com>
2020-04-20 22:52:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let correctAnswer = "silenzio";
+let closeAnswer = "silence";
+let catImage = document.getElementById("catImage");
+let inputField = document.getElementById("inputField");
+let input = "";
+
+
+console.log(inputField);
+
+function catify(){
+    input = inputField.value;
+    if(input == "silenzio"){
+        alert("123456789kitten")
+    }
+    else if (input == "silence"){
+        alert(https://www.youtube.com/watch?v=UEnzE-NKa9A);
+    }
+    else
+    alert("https://www.youtube.com/watch?v=ZXsQAXx_ao0");
+   
+}

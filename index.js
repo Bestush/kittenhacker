39ebff1cca4bfc85855122193c8838d98b894669
Bestun Gardi <bestun95@yahoo.com>
2020-04-20 23:37:18 +0300

    added youtube link with quotes

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function catify(){
         alert("637520787d9ef8dfd04dc3a5bbbb95b87876161142d91a0e3913d478a49e56c0")
     }
     else if (input == "silence"){
-        alert(https://www.youtube.com/watch?v=UEnzE-NKa9A);
+        alert("https://www.youtube.com/watch?v=UEnzE-NKa9A");
     }
-    else
-    alert("https://www.youtube.com/watch?v=gJscrxxl_Bg");
+    else{
+    alert("https://www.youtube.com/watch?v=gJscrxxl_Bg");}
    
 }
